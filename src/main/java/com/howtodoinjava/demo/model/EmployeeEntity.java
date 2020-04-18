3c59a50824e6e4aca2c4a74f3e2c8f1e75ceb1d8
Vamshi Kapidi <vamshikpd@gmail.com>
2020-04-18 13:02:42 -0700

    Spring boot JPA

---
+++ src/main/java/com/howtodoinjava/demo/model/EmployeeEntity.java
@@ -0,0 +1,64 @@
+package com.howtodoinjava.demo.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="TBL_EMPLOYEES")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    
+    @Column(name="first_name")
+    private String firstName;
+    
+    @Column(name="last_name")
+    private String lastName;
+    
+    @Column(name="email", nullable=false, length=200)
+    private String email;
+    
+    public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+    @Override
+    public String toString() {
+        return "EmployeeEntity [id=" + id + ", firstName=" + firstName + 
+                ", lastName=" + lastName + ", email=" + email   + "]";
+    }
+}
